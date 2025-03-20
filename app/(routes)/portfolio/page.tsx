498ac4522f29cbcfc0e08f40374d29006edce896
jbaxax <walterjave92@gmail.com>
2025-03-20 15:59:21 -0500

    ✅ update component portfolio

--- app/(routes)/portfolio/page.tsx
+++ app/(routes)/portfolio/page.tsx
@@ -2,7 +2,10 @@ import PortfolioBox from "./components/portfolio-box";
 
 export default function Portfolio() {
   return (
-    <div className="flex justify-center items-center h-screen w-full ">
+    <div className="flex flex-col justify-center items-center md:h-screen w-full mt-24 md:mt-0 ">
+      <div>
+        <h1 className="4xl lg:text-7xl mb-4">Proyectos</h1>
+      </div>
       <PortfolioBox/>
     </div>
   )
