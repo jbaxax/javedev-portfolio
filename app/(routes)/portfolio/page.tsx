b0f0f116ac5ca91fcd27473dfd264f3c65d4c2e7
walter259 <walterjave60@gmail.com>
2025-07-03 13:39:09 -0500

    Add proyect mundo novelas web

--- app/(routes)/portfolio/page.tsx
+++ app/(routes)/portfolio/page.tsx
@@ -3,9 +3,7 @@ import PortfolioBox from "./components/portfolio-box";
 export default function Portfolio() {
   return (
     <div className="flex flex-col justify-center items-center md:h-screen w-full mt-24 md:mt-0 ">
-      <div>
-        <h1 className="4xl lg:text-7xl mb-4">Proyectos</h1>
-      </div>
+      
       <PortfolioBox/>
     </div>
   )
